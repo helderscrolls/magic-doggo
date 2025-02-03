72ecde21dcb0bac57554af34408400b45daf1821
helderscrolls <nortehelder@gmail.com>
2025-02-04 00:45:55 +0100

    feat(player): add blink effect to player when hit

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,11 +6,16 @@ signal healthChanged
 
 @export var speed: int = 35
 @onready var animations = $AnimationPlayer
+@onready var effects = $Effects
+@onready var hurtTimer = $HurtTimer
 
 @export var maxHealth = 3
 @onready var currentHealth: int = maxHealth
 
 @export var knockbackPower: int = 500
+
+func _ready():
+	effects.play("RESET")
 
 func handleInput():
 	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
@@ -52,6 +57,10 @@ func _on_hurt_box_area_entered(area: Area2D) -> void:
 			
 		healthChanged.emit(currentHealth)
 		knockback(area.get_parent().velocity)
+		effects.play("hurtBlink")
+		hurtTimer.start()
+		await hurtTimer.timeout
+		effects.play("RESET")
 
 func knockback(enemyVelocity: Vector2):
 	var knockbackDirection = (enemyVelocity - velocity).normalized() * knockbackPower
