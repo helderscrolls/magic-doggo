f1a074886bbdfc5c30a1e75503d79a87b9126787
helderscrolls <nortehelder@gmail.com>
2025-02-14 20:58:31 +0100

    refactor(hotbar): initial logic to equip weapon instead of consuming it

---
+++ inventory/items/scripts/weapon_item.gd
@@ -0,0 +1,11 @@
+class_name WeaponItem extends InventoryItem
+
+@export var weapon_class: PackedScene = preload("res://player/scenes/big_sword.tscn")
+
+var weapon
+
+func _init() -> void:
+	weapon = weapon_class.instantiate()
+
+func use(player: Player) -> void:
+	pass
